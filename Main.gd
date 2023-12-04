af23cdabb4d603ea8431981e87bce8412bacff71
Marco Betschart <email@marco.betschart.name>
2023-12-04 21:50:51 +0100

    OpenXR + Handtracking

---
+++ Main.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+var xr_interface: XRInterface
+
+func _ready():
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+
+		# Turn off v-sync!
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+		# Change our main viewport to output to the HMD
+		get_viewport().use_xr = true
+		
+		if enable_passthrough():
+			print("XR passthrough enabled")
+		else:
+			print("XR passthrough could not be enabled")
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
+
+func enable_passthrough() -> bool:
+	if xr_interface and xr_interface.is_passthrough_supported():
+		if !xr_interface.start_passthrough():
+			return false
+	else:
+		var modes = xr_interface.get_supported_environment_blend_modes()
+		if xr_interface.XR_ENV_BLEND_MODE_ALPHA_BLEND in modes:
+			xr_interface.set_environment_blend_mode(xr_interface.XR_ENV_BLEND_MODE_ALPHA_BLEND)
+		else:
+			return false
+
+	get_viewport().transparent_bg = true
+	return true
